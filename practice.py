4878f09dd967286003b33a0cc4b383bfdbf6ab12
Shivansh Chhabra <20603692+shan18u@users.noreply.github.com>
2023-01-10 12:52:11 -0800

    final comimts.

--- practice.py
+++ practice.py
@@ -1,20 +1,21 @@
-from curses 
-import wrapper
+import curses
+from curses import wrapper
 import queue
 import time
 
-
 maze = [
-    ["/", "O", "/", "/", "/", "/", "/", "/", "/"],
-    ["/", " ", " ", " ", " ", " ", " ", " ", "/"],
-    ["/", " ", "#", "#", " ", "#", "#", " ", "/"],
-    ["/", " ", "#", " ", " ", " ", "#", " ", "/"],
-    ["/", " ", "#", " ", "#", " ", "#", " ", "/"],
-    ["/", " ", "#", " ", "#", " ", "#", " ", "/"],
-    ["/", " ", "#", " ", "#", " ", "#", "#", "/"],
-    ["/", " ", " ", " ", " ", " ", " ", " ", "/"],
-    ["/", "/", "/", "/", "/", "/", "/", "X", "/"]
+    ["#", "O", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", " ", "#", "#", " ", "#", "#", " ", "#"],
+    ["#", " ", "#", " ", " ", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", " ", "#"],
+    ["#", " ", "#", " ", "#", " ", "#", "#", "#"],
+    ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
+
+
 def print_maze(maze, stdscr, path=[]):
     BLUE = curses.color_pair(1)
     RED = curses.color_pair(2)
@@ -27,7 +28,6 @@ def print_maze(maze, stdscr, path=[]):
                 stdscr.addstr(i, j*2, value, BLUE)
 
 
-
 def find_start(maze, start):
     for i, row in enumerate(maze):
         for j, value in enumerate(row):
@@ -35,6 +35,42 @@ def find_start(maze, start):
                 return i, j
 
     return None
+
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
+    q.put((start_pos, [start_pos]))
+
+    visited = set()
+
+    while not q.empty():
+        current_pos, path = q.get()
+        row, col = current_pos
+
+        stdscr.clear()
+        print_maze(maze, stdscr, path)
+        time.sleep(0.2)
+        stdscr.refresh()
+
+        if maze[row][col] == end:
+            return path
+
+        neighbors = find_neighbors(maze, row, col)
+        for neighbor in neighbors:
+            if neighbor in visited:
+                continue
+
+            r, c = neighbor
+            if maze[r][c] == "#":
+                continue
+
+            new_path = path + [neighbor]
+            q.put((neighbor, new_path))
+            visited.add(neighbor)
 
 
 def find_neighbors(maze, row, col):
